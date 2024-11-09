77a3650182cbe3532480b409767194474ee2ad6a
Beruta <biruktawitshiferaw522@gmail.com>
2024-11-09 12:22:20 +0300

    authentication

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,22 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import styles from "./page.module.css";
 
 function Dashboard() {
-  return <div></div>;
+  const router = useRouter();
+  const session = useSession();
+
+  if (session.status === "loading") {
+    <h1>Loading...</h1>;
+  }
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+  if (session.status === "authenticated") {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 }
 
 export default Dashboard;
